cf5a04e94977feb80fb3e627c3544984a430d691
Jung Lee <lee.jung.s95@gmail.com>
2020-06-23 03:03:28 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -69,8 +69,8 @@ class MusicLibraryController
     #once the user input is entered, the method should shoutout the corresponding
     #index's song and its artist
     binding.pry
-    if list_songs.each {|x| x}.include?(user_input)
-
+    if list_songs.each{|x| x}.include?(user_input)
+      
     #puts "Playing #{song.name} by #{song.artist.name}"
   end
 

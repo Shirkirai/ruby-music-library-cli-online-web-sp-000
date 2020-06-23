c01a370c9093a7d7b278bcf74dd1e573cd81ab87
Jung Lee <lee.jung.s95@gmail.com>
2020-06-23 03:12:34 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -68,7 +68,7 @@ class MusicLibraryController
     #user input should be an integer ranging from 1 to 4
     #once the user input is entered, the method should shoutout the corresponding
     #index's song and its artist
-    binding.pry
+    
     if (1..Song.all.length).include?(user_input)
       song = Song.all.sort_by(&:name)[user_input - 1]
     end
